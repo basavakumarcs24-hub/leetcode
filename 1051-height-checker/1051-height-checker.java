985b7af2558a4a121086c7f751046cbeb71f1d22
basavakumarcs24-hub <basavakumar.cs24@bmsce.ac.in>
2026-09-24 23:52:29 +0530

    Time: 1 ms (92.42%), Space: 43.5 MB (26.76%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,82 @@
+
+import java.util.Arrays;
+
+class Solution {
+
+    public int heightChecker(int[] heights) {
+
+        int count = 0;
+
+        int[] dupe = Arrays.copyOf(heights, heights.length);
+
+        // right is inclusive
+        sort(dupe, 0, dupe.length - 1);
+
+        for (int i = 0; i < dupe.length; i++) {
+            if (dupe[i] != heights[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void sort(int[] arr, int left, int right) {
+
+        if (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            sort(arr, left, mid);
+            sort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            leftArr[i] = arr[left + i];
+        }
+
+        for (int j = 0; j < n2; j++) {
+            rightArr[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0;
+        int j = 0;
+        int k = left;
+
+        while (i < n1 && j < n2) {
+
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+    }
+}
